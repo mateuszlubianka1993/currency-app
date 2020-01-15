4d724730ae26dab2245a261a19acd2e31d7dc24d
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-01-15 20:20:50 +0100

    Add jquery config and app.js file

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,11 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    resolve: {
+        alias: {
+            jquery: "jquery/src/jquery"
+        }
+    },
     module: {
         rules: [
           {
